bc8c430d3ad7457d29987f5555a3a0d3cc9bf40b
Arishaba-R <87789153+Arishaba-R@users.noreply.github.com>
2021-07-26 15:54:30 -0500

    Add files via upload

---
+++ Anagram Function QA.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Anagram Function QA
+
+# In[1]:
+
+
+def Anagram_1( word_a, word_b):
+    word_a_sorted =sorted(word_a)
+    word_b_sorted =sorted(word_a)
+ 
+    if word_a_sorted == word_b_sorted:
+        print("word_a, and word_b are anagrams")
+        
+    else:
+        print("word_a, and word_b are anagrams")
+
+
+# In[ ]:
+
+
+
+
